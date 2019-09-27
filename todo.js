b859af78e81c2425b6f9ae333d5e2052fa8f50a6
hwang1449 <53260005+hwang1449@users.noreply.github.com>
2019-09-27 17:05:53 +0900

    todo.js-deleteToDo

--- todo.js
+++ todo.js
@@ -14,8 +14,11 @@ function deleteToDo(event) {
     const btn = event.target;
     const li = btn.parentNode;
     toDoList.removeChild(li);
-    const cleanToDos = toDos.filter(filterFn);
-    console.log(cleanToDos);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    saveToDos();   
 }
 
 function saveToDos() {
@@ -28,8 +31,8 @@ function paintToDo(text) {
     const span = document.createElement("span");
     const newId = toDos.length + 1;
     delBtn.innerText = "X";
-    delBtn.innerText = text;
     delBtn.addEventListener("click", deleteToDo);
+    delBtn.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
     li.id = newId;
@@ -51,7 +54,7 @@ function handleSubmit(event) {
 
 function loadToDos() {
     const loadedToDos = localStorage.getItem(TODOS_LS);
-    if(loadedToDos !== null) {
+    if (loadedToDos !== null) {
         const parsedToDos = JSON.parse(loadedToDos);
         parsedToDos.forEach(function(toDo) {
             paintToDo(toDo.text);
