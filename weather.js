95fcff1e1da299d256427bbf5d1b9ce4fe6bd34c
hwang1449 <53260005+hwang1449@users.noreply.github.com>
2019-09-29 02:20:53 +0900

    yet

--- weather.js
+++ weather.js
@@ -1,17 +1,21 @@
-const dweather = document.querySelector(".js-weather");
+const weather = document.querySelector(".js-weather");
 
 const API_KEY ="052b7cb0d7b354ecc03d8354c6d9475c";
 const COORDS = "coords";
 
 function getWeather(lat, lng) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`)
+    fetch(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    )
     .then(function(response) {
         return response.json();
-    }).then(function(json){
+    })
+    .then(function(json) {
         const temperature = json.main.temp;
         const place = json.name;
         weather.innerText = `${temperature} @ ${place}`;
     });
+}    
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -44,7 +48,6 @@ function loadCoords() {
         const parsedCoords = JSON.parse(loadedCoords);
         getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
-        
 }
 
 function init() {
